3bafdda12ddcb7826864c7307adf4a92c34bca14
STYJ <simon.tan.yu.jing@gmail.com>
2021-01-18 11:21:49 +0800

    added stub ethereum.py module

---
+++ ethereum.py
@@ -0,0 +1,25 @@
+from web3 import Web3
+from dotenv import load_dotenv
+import os
+
+# Environtment variables
+load_dotenv()
+
+# Infura Path
+PATH = 'wss://mainnet.infura.io/ws/v3/'
+
+def connect_to_web3():
+    KEY = os.getenv("INFURA_KEY")
+    w3 = Web3(Web3.WebsocketProvider(PATH + KEY))
+    try:
+        w3.isConnected()
+        return w3
+    except:
+        print('Failed to connect to web3 provider.')
+        exit(1)
+
+def get_latest_block(w3):
+    return w3.eth.blockNumber
+
+w3 = connect_to_web3()
+print(get_latest_block(w3))
