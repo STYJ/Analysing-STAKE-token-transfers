0109bb9a2b4560d5b4dfdd41837ecb11b930fed6
STYJ <simon.tan.yu.jing@gmail.com>
2021-01-19 19:02:23 +0800

    updated token dictionary format

--- token_details.py
+++ token_details.py
@@ -1,4 +1,6 @@
-STAKE_TOKEN_DETAILS = {
-    'address': '0x0Ae055097C6d159879521C384F1D2123D1f195e6',
-    'start': 9877421
+TOKEN_DETAILS = {
+    'stake': {
+        'address': '0x0Ae055097C6d159879521C384F1D2123D1f195e6',
+        'start': 9877421
+    }
 }
