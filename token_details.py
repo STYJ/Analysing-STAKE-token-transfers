2c82cead7b4b4c135092404cb7a568158ca29d81
STYJ <simon.tan.yu.jing@gmail.com>
2021-01-20 14:33:53 +0800

    added interval into token details

--- token_details.py
+++ token_details.py
@@ -1,6 +1,7 @@
 TOKEN_DETAILS = {
     'stake': {
         'address': '0x0Ae055097C6d159879521C384F1D2123D1f195e6',
-        'start': 9877421
+        'start': 9877421,
+        'interval': 2500
     }
 }
