09d95f208a31ef9d857f5d4616a4431b26f6563c
STYJ <simon.tan.yu.jing@gmail.com>
2021-01-20 12:23:43 +0800

    synchronising latest code

--- ethereum.py
+++ ethereum.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 import os
 import json
 from token_details import TOKEN_DETAILS as td
+import csv
+import pandas as pd
 
 # Environtment variables
 load_dotenv()
@@ -39,6 +41,17 @@ def get_token_instance(w3, name):
         print(f'"{name}" is not a valid token symbol')
         exit(1)
 
+# TODO: Maybe next time you don't have to save as CSV.
+# Just parse directly with pandas
+# TODO: Should also include block number in file name
+def save_to_csv(nameOfFile, data):
+    with open(nameOfFile, 'w', encoding='utf8', newline='') as output_file:
+        fc = csv.DictWriter(output_file, fieldnames=data[0].keys())
+        fc.writeheader()
+        fc.writerows(data)
+
+
+
 
 w3 = connect_to_web3()
 tokenName = 'stake'
@@ -46,14 +59,20 @@ tokenInstance = get_token_instance(w3, tokenName)
 decimals = 10 ** tokenInstance.functions.decimals().call()
 # fromBlock = td.get(tokenName.lower()).get('start')
 # toBlock = get_latest_block(w3)
-
 fromBlock = 11685382
-toBlock = 11685385
+toBlock = 11685500
+
 event_filter = tokenInstance.events.Transfer.createFilter(
     fromBlock=fromBlock,
     toBlock=toBlock
 )
-print(event_filter.get_all_entries()[0].get('args'))
+
+txns = [x.args for x in event_filter.get_all_entries()]
+df = pd.DataFrame(txns)
+print(df.head())
+
+# save_to_csv('txns.csv', txns)
+
 
 
 # print(get_latest_block(w3))
