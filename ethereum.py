6dbf4893046162b27bf6cca576a24bf875a91e3e
STYJ <simon.tan.yu.jing@gmail.com>
2021-01-21 15:21:42 +0800

    added logic for querying uniswap events

--- ethereum.py
+++ ethereum.py
@@ -2,7 +2,7 @@ from web3 import Web3
 from dotenv import load_dotenv
 import os
 import json
-from token_details import TOKEN_DETAILS as td
+from constants import TOKEN_DETAILS as td
 import csv
 import pandas as pd
 
@@ -28,20 +28,12 @@ def get_latest_block(w3):
     return w3.eth.blockNumber
 
 
-def get_erc20_abi():
-    with open('contracts/IERC20.json') as f:
+def get_abi(name):
+    with open('ABIs/' + name) as f:
         return json.load(f)
 
-
-def get_token_instance(w3, name):
-    address = td.get(name.lower(), False)
-    if address:
-        address = address.get('address')
-        abi = get_erc20_abi()
-        return w3.eth.contract(address=address, abi=abi)
-    else:
-        print(f'"{name}" is not a valid token symbol')
-        exit(1)
+def get_contract_instance(w3, address, abi):
+    return w3.eth.contract(address=address, abi=abi)
 
 
 def get_last_block(name_of_file):
@@ -62,26 +54,21 @@ def extract_info(txn):
     retVal['block'] = txn.blockNumber
     return retVal
 
-
-def get_transfers(instance, from_block, to_block):
-    event_filter = instance.events.Transfer.createFilter(
+def get_events(instance, event_name, from_block, to_block):
+    event_filter = instance.events[event_name].createFilter(
         fromBlock=from_block,
         toBlock=to_block
     )
     return [extract_info(x) for x in event_filter.get_all_entries()]
 
 
-def get_historical_txns(w3, name, contract, path, query_function, end=0, is_new=False):
-    details = td.get(name)
-    interval = details.get('interval')
+# When you want to query historical transactions for the first time, you should specify
+# the start_block and is_new = true
+def get_historical_txns(w3, name, contract, path, event_name, start=0, end=0, interval=1000, is_new=False):
 
     # If file is not new, get the latest row and extract the last block.
-    # Else get the start block from event_details.py
-    start = 0
     if not is_new:
         start = int(get_last_block(path) + 1)
-    else:
-        start = details.get('start')
 
     # Check if end block is provided.
     if not end:
@@ -96,17 +83,69 @@ def get_historical_txns(w3, name, contract, path, query_function, end=0, is_new=
         if(to_block > end_block):
             to_block = end_block
         print(from_block, to_block)
-        txns.extend(query_function(contract, from_block, to_block))
+        txns.extend(get_events(contract, event_name, from_block, to_block))
         from_block += interval
         to_block += interval
 
-    save_to_csv(path, txns, write_header=is_new)
+    if len(txns) > 0:
+        save_to_csv(path, txns, write_header=is_new)
 
 
 w3 = connect_to_web3()
-token_instance = get_token_instance(w3, 'stake')
-get_historical_txns(w3=w3,
-                    name='stake',
-                    contract=token_instance,
-                    path='./stake/transfers.csv',
-                    query_function=get_transfers)
+
+token_name = 'stake'
+token_details = td.get(token_name)
+
+# abi = get_abi('IERC20.json')
+# address = token_details.get('erc20').get('address')
+# instance = get_contract_instance(w3, address, abi)
+
+# get_historical_txns(w3=w3,
+#                     name=token_name,
+#                     contract=instance,
+#                     path='./stake/transfers.csv',
+#                     event_name=token_details.get('erc20').get('event_name'),
+#                     interval=token_details.get('erc20').get('interval'))
+
+# abi = get_abi('BPool.json')
+# address = token_details.get('balancer').get('address')
+# instance = get_contract_instance(w3, address, abi)
+
+# get_historical_txns(w3=w3,
+#                     name=token_name,
+#                     contract=instance,
+#                     path='./stake/join_balancer.csv',
+#                     event_name=token_details.get('balancer').get('event_name')[0],
+#                     interval=token_details.get('balancer').get('interval'),
+#                     is_new=False)
+
+
+# get_historical_txns(w3=w3,
+#                     name=token_name,
+#                     contract=instance,
+#                     path='./stake/exit_balancer.csv',
+#                     event_name=token_details.get('balancer').get('event_name')[1],
+#                     start=token_details.get('balancer').get('start'),
+#                     interval=token_details.get('balancer').get('interval'),
+#                     is_new=True)
+                    
+abi = get_abi('UniswapV2Pair.json')
+address = token_details.get('uniswap').get('address')
+instance = get_contract_instance(w3, address, abi)
+
+for e in token_details.get('uniswap').get('event_name'):
+    get_historical_txns(w3=w3,
+                        name=token_name,
+                        contract=instance,
+                        path=f'./stake/{e.lower()}_uniswap.csv',
+                        event_name=e,
+                        interval=token_details.get('uniswap').get('interval'))
+
+# get_historical_txns(w3=w3,
+#                     name=token_name,
+#                     contract=instance,
+#                     path='./stake/burn_uniswap.csv',
+#                     event_name=token_details.get('uniswap').get('event_name')[1],
+#                     start=token_details.get('uniswap').get('start'),
+#                     interval=token_details.get('uniswap').get('interval'),
+#                     is_new=True)
