75b98c702a2ab713622514a6b558e37044dd253c
STYJ <simon.tan.yu.jing@gmail.com>
2021-01-21 15:36:06 +0800

    updated address for balancer pool and added notes on how to parse csv

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+# For balancer events, the tokenAmount(In/Out) tells you how many tokens
+# For uniswap events, amount0 represents tokens, amount1 represents eth
 TOKEN_DETAILS = {
     'stake': {
         'erc20': {
@@ -7,7 +9,7 @@ TOKEN_DETAILS = {
             'event_name': 'Transfer',
         },
         'balancer': {
-            'address': '0x0Ae055097C6d159879521C384F1D2123D1f195e6',
+            'address': '0x1DDF0976Ac842C696d01a86b39d25b067Ed8C7ff',
             'start': 11281296,
             'interval': 100000,
             'event_name': ['LOG_JOIN', 'LOG_EXIT'],
